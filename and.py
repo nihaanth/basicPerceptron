0e903ae5380f89bc96504e171521d06a1a20644c
nihaanth <nihaanthreddy3@gmail.com>
2023-08-24 23:37:32 +0530

    code organised

--- and.py
+++ and.py
@@ -4,10 +4,15 @@ from all_utils import prepare_data
 AND = {
     'x1':[0,0,1,1],
     'x2':[0,1,0,1],
-    'y':[0,0,0,1],
+    'y':[0,0,0,1],  
 }
 
 df = pd.DataFrame(AND)
 
-prepare_data(df)
+print(df)
 
+model = Perceptron(eta=ETA,epochs=EPHOCH)
+model.fit(X,y)
+
+if __name__ == '__main__':#entry point
+    main()
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -1,5 +1,5 @@
 ##contains helper functions
-
+from models import Perceptron
 def prepare_data(df):
   X = df.drop('y', axis = 1)
 
